848c9e8c4c20f3ff39b3aaf8ca374cf96916d2bc
Wojciech <wojciech.kur96@gmail.com>
2020-04-04 19:59:31 +0200

    Not working - yet

--- Common/FileWriter.py
+++ Common/FileWriter.py
@@ -1,5 +1,20 @@
+from enum import Enum
+from typing import List
+from treelib import Node, Tree
+
+
+class FileMode(Enum):
+    BASIC = 1
+    EXTENDED = 2
 
 
 class FileWriter:
-    def __init__(self):
-        pass
+    def __init__(self, file):
+        self.__file = file
+
+    def write(self, tree: Tree):
+        nodes: List[Node] = tree.leaves('root')
+        print(nodes)
+        print(nodes[0])
+        subtree: Tree = tree.subtree(nodes[0].identifier)
+        subtree.show()
--- Run.py
+++ Run.py
@@ -1,10 +1,12 @@
 from Core.JTExt import JTExt
+from Common.FileWriter import FileWriter
 
 
 if __name__ == '__main__':
     if __name__ == '__main__':
         parser = JTExt()
         prob = JTExt.generate_test_json()
-        print(prob)
         tr3 = parser.parse_json(prob)
         tr3.show()
+        writer = FileWriter('file')
+        writer.write(tr3)
