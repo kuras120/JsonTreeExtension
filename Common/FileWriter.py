4d9b31329170b4015a10d71569210a66d3cba38c
Wojciech <wojciech.kur96@gmail.com>
2020-04-24 16:44:44 +0200

    Concept

--- Common/FileWriter.py
+++ Common/FileWriter.py
@@ -12,9 +12,12 @@ class FileWriter:
     def __init__(self, file):
         self.__file = file
 
+    # parent, siblings (rodzenstwo), paths_to_leaves
     def write(self, tree: Tree):
         nodes: List[Node] = tree.leaves('root')
         print(nodes)
         print(nodes[0])
-        subtree: Tree = tree.subtree(nodes[0].identifier)
-        subtree.show()
+        parent: Node = tree.parent(nodes[0].identifier)
+        print(parent)
+        paths: list = tree.paths_to_leaves()
+        print(paths)
